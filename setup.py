34c17f349dc744ae815517a0787f47575598bff6
EvATive7 <evative7@gmail.com>
2024-05-10 19:19:55 +0800

    chore: ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="easywebhooker",
-    version="0.0.1",
+    version="0.0.2",
     author="EvATive7",
     author_email="",
     long_description=long_description,
